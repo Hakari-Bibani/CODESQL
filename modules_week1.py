9e1d53031d08e92ce045a97f29f2de0b17c7767d
Hakari Bibani <hakary.jalal33@gmail.com>
2025-02-06 15:26:18 +0300

    Create modules_week1.py

---
+++ modules_week1.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.title("Week 1: Introduction to Coding")
+    st.write("This week covers the basics of coding. Get ready to dive into fundamental programming concepts!")
