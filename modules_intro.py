1fc22cbe949801499daf1dea03910cc288aadd7f
Hakari Bibani <hakary.jalal33@gmail.com>
2025-02-06 15:25:23 +0300

    Create modules_intro.py

---
+++ modules_intro.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.title("Introduction")
+    st.write("Welcome to the introduction module. Here you'll get an overview of the course and what to expect.")
