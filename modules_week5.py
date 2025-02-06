f3137453f5f77fd50a9ce4664ba0153a9061819d
Hakari Bibani <hakary.jalal33@gmail.com>
2025-02-06 15:28:03 +0300

    Create modules_week5.py

---
+++ modules_week5.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.title("Week 5: Finalizing and Showcasing Your Personalized Project")
+    st.write("In the final week, you'll polish your project and learn how to showcase your work to the world!")
