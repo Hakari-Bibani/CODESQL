cd019c3cd3547c51e21278a85ee0724282917388
Hakari Bibani <hakary.jalal33@gmail.com>
2025-02-06 15:27:37 +0300

    Create modules_week4.py

---
+++ modules_week4.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.title("Week 4: Data Week")
+    st.write("Explore data handling, analysis, and visualization techniques during Data Week. Get hands-on with real datasets!")
