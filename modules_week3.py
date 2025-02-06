057f25f78b2f8dc2f0759bdcb44e5ca9f03b6185
Hakari Bibani <hakary.jalal33@gmail.com>
2025-02-06 15:27:16 +0300

    Create modules_week3.py

---
+++ modules_week3.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.title("Week 3: Deploy App through Github and Streamlit")
+    st.write("Learn how to deploy your applications using Github and Streamlit. Follow the steps to get your app online!")
