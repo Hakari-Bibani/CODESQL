be0a7d951087f2113e6c9a3b7e2b5ae420439bb5
Hakari Bibani <hakary.jalal33@gmail.com>
2025-02-07 20:36:34 +0300

    Update modules_intro.py

--- modules_intro.py
+++ modules_intro.py
@@ -33,8 +33,9 @@ def show():
         """)
     
     with tab3:
-         st.title("<span style='color: #FFDAB9;'>Stay Connected: Join Our Discord Server!</span>",unsafe_allow_html=True)
-         st.markdown("""**Enhance your learning experience by joining our Discord server**, a dedicated space for collaboration, support, and community engagement. """)
+        st.title("Stay Connected: Join Our Discord Server!")
+        st.markdown("""
+        **Enhance your learning experience by joining our Discord server**, a dedicated space for collaboration, support, and community engagement.
 
         ## Why Join Our Discord Server?
         * **Stay Updated:** Get instant updates about the course, assignments, and materials.
