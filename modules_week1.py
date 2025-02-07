42917105e3fa90d105c2ce07906578d6b97700dc
Hakari Bibani <hakary.jalal33@gmail.com>
2025-02-07 20:52:03 +0300

    Update modules_week1.py

--- modules_week1.py
+++ modules_week1.py
@@ -27,9 +27,9 @@ with tab1:
     st.markdown("[Links to an external site](https://chatgpt.com/share/6733c214-7ac4-8004-92f1-227d11b644ff)")
     st.subheader("**Content**:")
     st.write(
-        "In this session, we'll introduce you to the basics of Python and how it can be a powerful tool for enhancing personal impact, "
-        "whether you're looking to automate tasks, analyze data, or create small projects. We will cover foundational topics such as "
-        "setting up your Python environment, understanding Python syntax, and exploring the practical applications of Python in everyday scenarios."
+    "In this session, we'll introduce you to the basics of Python and how it can be a powerful tool for enhancing personal impact, "
+    "whether you're looking to automate tasks, analyze data, or create small projects. We will cover foundational topics such as "
+    "setting up your Python environment, understanding Python syntax, and exploring the practical applications of Python in everyday scenarios."
     )
 
     with tab2:
